daa8515bbfb17cd688026ea0b1572ad251b509c2
Michal Jenis <michal.jenis@kosik.cz>
2022-01-09 12:56:25 +0100

    Add function App\Utils\Files::writingToAFile

---
+++ src/App/Utils/Files.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Utils;
+
+class Files
+{
+
+	/**
+	 * @param string $filename
+	 * @param string $text
+	 * @return void
+	 */
+	public static function writingToAFile(string $filename, string $text): bool
+	{
+		if (file_exists($filename) === false) {
+			return false;
+		}
+
+		$myFile = fopen($filename, 'ab+');
+		if ($myFile === false) {
+			return false;
+		}
+		if (fwrite($myFile, $text) === false) {
+			return false;
+		}
+		if (fclose($myFile) === false) {
+			return false;
+		}
+
+		return true;
+	}
+
+}
